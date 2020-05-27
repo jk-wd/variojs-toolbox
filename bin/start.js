5e0e82cdfede8e675da62f36dce30b759e3f3f69
Joris Kuijper <joriskuijper@Joriss-MacBook-Pro.local>
2020-05-27 19:42:54 +0200

    fixed path to opencli in bin/start

--- bin/start.js
+++ bin/start.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 var path = require("path");
-console.log(`node ${path.join(__dirname, '../src/server/')}index.js`);
+
 shell.exec(`node ${path.join(__dirname, '../src/server/')}index.js`, {async:true});
 shell.exec(`node ${path.join(__dirname, '../src/socket-server/')}index.js`, {async:true});
-shell.exec(`${path.join(__dirname, 'node_modules/.bin/')}open-cli http://localhost:8080`);
+shell.exec(`${path.join(__dirname, '../node_modules/.bin/')}open-cli http://localhost:8080`);
