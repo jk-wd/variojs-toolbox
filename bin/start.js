f3143e5d2d695a5a6e491eef6a8fd4bf19ed9172
Joris Kuijper <joriskuijper@Joriss-MacBook-Pro.local>
2020-05-27 16:25:23 +0200

    updated package name in bin start script

--- bin/start.js
+++ bin/start.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
-shell.exec("node ./node_modules/variojs-dev-tools/src/server/index.js", {async:true});
-shell.exec("node ./node_modules/variojs-dev-tools/src/socket-server/index.js", {async:true});
-shell.exec("./node_modules/variojs-dev-tools/node_modules/.bin/open-cli http://localhost:8080");
+shell.exec("node ./node_modules/variojs-toolbox/src/server/index.js", {async:true});
+shell.exec("node ./node_modules/variojs-toolbox/src/socket-server/index.js", {async:true});
+shell.exec("./node_modules/variojs-toolbox/node_modules/.bin/open-cli http://localhost:8080");
